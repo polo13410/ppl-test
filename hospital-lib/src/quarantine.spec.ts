c58134c4b4540ac6419ec9d2c196017b84416ea1
Polo13410 <polo13410@hotmail.fr>
2022-11-25 15:16:24 +0100

    dev: lib
    Implementing methods
    Editing tests

--- hospital-lib/src/quarantine.spec.ts
+++ hospital-lib/src/quarantine.spec.ts
@@ -1,10 +1,15 @@
-import {Expect, Setup, Test, TestFixture} from 'alsatian';
-import {Quarantine} from './quarantine';
+import { Expect, Setup, Test, TestFixture } from "alsatian";
+import { Quarantine } from "./quarantine";
+import { PatientsRegister } from "./patientsRegister";
+
+interface testObj {
+  input: Quarantine;
+  expected: PatientsRegister[];
+}
 
 @TestFixture()
 export class QuarantineTest {
-
-  private quarantine: Quarantine;
+  private quarantines: testObj[];
 
   @Setup
   public setup() {
@@ -17,10 +22,48 @@ export class QuarantineTest {
     // F : Fever
     // D : Diabetes
     // T : Tuberculosis
+    this.quarantines = [
+      {
+        input: new Quarantine({ F: 1, H: 2, D: 3, T: 1, X: 0 }),
+        expected: [
+          { F: 1, H: 2, D: 3, T: 1, X: 0 },
+          { F: 1, H: 2, D: 0, T: 1, X: 3 },
+          { F: 0, H: 3, D: 0, T: 1, X: 3 },
+          { F: 1, H: 3, D: 0, T: 0, X: 3 },
+          { F: 1, H: 2, D: 3, T: 1, X: 0 },
+          { F: 3, H: 1, D: 3, T: 0, X: 0 },
+          { F: 0, H: 3, D: 0, T: 1, X: 3 },
+          { F: 0, H: 0, D: 0, T: 0, X: 7 },
+        ],
+      },
+      {
+        input: new Quarantine({ F: 6, H: 4, D: 6, T: 1, X: 1 }),
+        expected: [
+          { F: 6, H: 4, D: 6, T: 1, X: 1 },
+          { F: 6, H: 4, D: 0, T: 1, X: 7 },
+          { F: 0, H:10, D: 0, T: 1, X: 7 },
+          { F: 6, H: 5, D: 0, T: 0, X: 7 },
+          { F: 6, H: 4, D: 6, T: 1, X: 1 },
+          { F: 10, H: 1, D: 6, T: 0, X: 1 },
+          { F: 0, H: 10, D: 0, T: 1, X: 7 },
+          { F: 0, H: 0, D: 0, T: 0, X: 18 },
+        ],
+      },
+      {
+        input: new Quarantine({ F: 0, H: 0, D: 0, T: 0, X: 10 }),
+        expected: [
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+          { F: 0, H: 0, D: 0, T: 0, X: 10 },
+        ],
+      },
+    ];
 
-    this.quarantine = new Quarantine({
-        F: 1, H: 2, D: 3, T: 1, X: 0
-    });
     // Quarantine provides medicines to the patients, but can not target a specific group of patient.
     // The same medicines are always given to all the patients.
 
@@ -30,79 +73,79 @@ export class QuarantineTest {
 
   @Test()
   public beforeTreatment(): void {
-    // diabetics die without insulin
-    Expect(this.quarantine.report()).toEqual({
-      F: 1, H: 2, D: 3, T: 1, X: 0
+    this.quarantines.forEach((quar) => {
+      Expect(quar.input.report()).toEqual(quar.expected[0]);
     });
   }
 
   @Test()
   public noTreatment(): void {
-    this.quarantine.wait40Days();
     // diabetics die without insulin
-    Expect(this.quarantine.report()).toEqual({
-      F: 1, H: 2, D: 0, T: 1, X: 3
+    this.quarantines.forEach((quar) => {
+      quar.input.wait40Days();
+      Expect(quar.input.report()).toEqual(quar.expected[1]);
     });
   }
 
   @Test()
   public aspirin(): void {
-    this.quarantine.setDrugs(['As']);
-    this.quarantine.wait40Days();
-    // aspirin cure Fever
-    Expect(this.quarantine.report()).toEqual({
-      F: 0, H: 3, D: 0, T: 1, X: 3
+    this.quarantines.forEach((quar) => {
+      quar.input.setDrugs(["As"]);
+      quar.input.wait40Days();
+      // aspirin cure Fever
+      Expect(quar.input.report()).toEqual(quar.expected[2]);
     });
   }
 
   @Test()
   public antibiotic(): void {
-    this.quarantine.setDrugs(['An']);
-    this.quarantine.wait40Days();
-    // antibiotic cure Tuberculosis
-    // but healthy people catch Fever if mixed with insulin.
-    Expect(this.quarantine.report()).toEqual({
-      F: 1, H: 3, D: 0, T: 0, X: 3
+    this.quarantines.forEach((quar) => {
+      quar.input.setDrugs(["An"]);
+      quar.input.wait40Days();
+      // antibiotic cure Tuberculosis
+      // but healthy people catch Fever if mixed with insulin.
+      Expect(quar.input.report()).toEqual(quar.expected[3]);
     });
   }
 
   @Test()
   public insulin(): void {
-    this.quarantine.setDrugs(['I']);
-    this.quarantine.wait40Days();
-    // insulin prevent diabetic subject from dying, does not cure Diabetes,
-    Expect(this.quarantine.report()).toEqual({
-      F: 1, H: 2, D: 3, T: 1, X: 0
+    this.quarantines.forEach((quar) => {
+      quar.input.setDrugs(["I"]);
+      quar.input.wait40Days();
+      // insulin prevent diabetic subject from dying, does not cure Diabetes,
+      Expect(quar.input.report()).toEqual(quar.expected[4]);
     });
   }
 
   @Test()
   public antibioticPlusInsulin(): void {
-    this.quarantine.setDrugs(['An', 'I']);
-    this.quarantine.wait40Days();
-    // if insulin is mixed with antibiotic, healthy people catch Fever
-    Expect(this.quarantine.report()).toEqual({
-      F: 3, H: 1, D: 3, T: 0, X: 0
+    this.quarantines.forEach((quar) => {
+      quar.input.setDrugs(["An", "I"]);
+      quar.input.wait40Days();
+      // if insulin is mixed with antibiotic, healthy people catch Fever
+      // startup => F: 1, H: 2, D: 3, T: 1, X: 0
+      Expect(quar.input.report()).toEqual(quar.expected[5]);
     });
   }
 
   @Test()
   public paracetamol(): void {
-    this.quarantine.setDrugs(['P']);
-    this.quarantine.wait40Days();
-    // paracetamol heals fever
-    Expect(this.quarantine.report()).toEqual({
-      F: 0, H: 3, D: 0, T: 1, X: 3
+    this.quarantines.forEach((quar) => {
+      quar.input.setDrugs(["P"]);
+      quar.input.wait40Days();
+      // paracetamol heals fever
+      Expect(quar.input.report()).toEqual(quar.expected[6]);
     });
   }
 
   @Test()
   public paracetamolAndAspirin(): void {
-    this.quarantine.setDrugs(['P', 'As']);
-    this.quarantine.wait40Days();
-    // paracetamol kills subject if mixed with aspirin
-    Expect(this.quarantine.report()).toEqual({
-      F: 0, H: 0, D: 0, T: 0, X: 7
+    this.quarantines.forEach((quar) => {
+      quar.input.setDrugs(["P", "As"]);
+      quar.input.wait40Days();
+      // paracetamol kills subject if mixed with aspirin
+      Expect(quar.input.report()).toEqual(quar.expected[7]);
     });
   }
 }
--- hospital-lib/src/quarantine.ts
+++ hospital-lib/src/quarantine.ts
@@ -1,22 +1,62 @@
-import {PatientsRegister} from './patientsRegister';
+import { PatientsRegister } from "./patientsRegister";
 
 export class Quarantine {
+  private static readonly NOT_IMPLEMENTED_MESSAGE = "Work, work.";
+  private pPatients: PatientsRegister = undefined;
+  private pDrugs: Array<string> = undefined;
 
-    private static readonly NOT_IMPLEMENTED_MESSAGE = 'Work, work.';
+  constructor(patients: PatientsRegister) {
+    this.pPatients = patients;
+  }
 
-    constructor(patients: PatientsRegister) {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
+  public setDrugs(drugs: Array<string>): void {
+    this.pDrugs = drugs;
+  }
+
+  public wait40Days(): void {
+    if (this.pDrugs === undefined || this.pDrugs.length === 0) {
+      this.setNewState("D", "X");
+      return;
     }
 
-    public setDrugs(drugs: Array<string>): void {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
+    if (
+      this.pDrugs.find((elem) => elem === "As") &&
+      this.pDrugs.find((elem) => elem === "P")
+    ) {
+      this.setNewState("F", "X");
+      this.setNewState("H", "X");
+      this.setNewState("D", "X");
+      this.setNewState("T", "X");
+    } else {
+      if (
+        this.pDrugs.find((elem) => elem === "As") ||
+        this.pDrugs.find((elem) => elem === "P")
+      ) {
+        this.setNewState("F", "H");
+      }
+      if (this.pDrugs.find((elem) => elem === "An")) {
+        if (this.pDrugs.find((elem) => elem === "I")) {
+          this.setNewState("H", "F");
+        }
+        this.setNewState("T", "H");
+      }
+      if (!this.pDrugs.find((elem) => elem === "I") /*No insulin*/) {
+        this.setNewState("D", "X");
+      }
     }
+  }
 
-    public wait40Days(): void {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
-    }
+  public report(): PatientsRegister {
+    return this.pPatients;
+  }
 
-    public report(): PatientsRegister {
-        throw new Error(Quarantine.NOT_IMPLEMENTED_MESSAGE);
+  //private methods
+  private setNewState(from: string, to: string) {
+    let numFrom = this.pPatients[from];
+    if (numFrom == 0) {
+      return;
     }
+    this.pPatients[to] += numFrom;
+    this.pPatients[from] = 0;
+  }
 }
